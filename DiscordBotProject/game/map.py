264bc6eedf0b4c6e2e9f94088cbf6b4583b30c0d
TheSnowyDodoAlt <cardenas.camilo@maximino.edu.co>
2026-05-28 08:22:49 -0500

    Update map.py

--- DiscordBotProject/game/map.py
+++ DiscordBotProject/game/map.py
@@ -1,3 +1,20 @@
 # the map structure
+# currently features 3 rooms for debugging
 
+ROOMS = {
+
+    "cafeteria": {
+        "front": "lounge",
+        "back": "storage"
+    },
+
+    "lounge": {
+        "back": "cafeteria"
+    },
+
+    "storage": {
+        "front": "cafeteria"
+    }
+
+}
 
