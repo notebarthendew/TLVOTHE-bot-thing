46ad819b2041316d9219b0452fd0f6ec363f874b
TheSnowyDodoAlt <cardenas.camilo@maximino.edu.co>
2026-06-05 11:12:01 -0500

    Update player.py

--- DiscordBotProject/game/player.py
+++ DiscordBotProject/game/player.py
@@ -2,7 +2,7 @@
 
 players = {}
 
-def create_player(user_id):
+def create_player(user_id, nickname):
 
     players[user_id] = {
         "room": "1",
@@ -10,6 +10,7 @@ def create_player(user_id):
         "role": None,
         "inventory": [],
         "status": [],
+        "nickname": nickname
     }
     print(f"Created player: {user_id}")
     print(players)
