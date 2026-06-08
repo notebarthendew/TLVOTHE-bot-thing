339fd052b8ff720debde89ec3bcd47c13369a89f
TheSnowyDodo <notebarthendew@gmail.com>
2026-06-07 19:54:19 -0500

    Update map.py

--- DiscordBotProject/game/map.py
+++ DiscordBotProject/game/map.py
@@ -7,43 +7,50 @@ from utils.constants import (
     ROOM3_ID,
     ROOM4_ID,
     ROOM5_ID,
-    ROOM6_ID
+    ROOM6_ID,
+    ROOM6_THREAD_ID
 )
 
 ROOMS = {
 
     "1": {
         "channel_id": ROOM1_ID,
+        "command_channel_id": ROOM1_ID,
         "front": None,
         "back": "2"
     },
 
     "2": {
         "channel_id": ROOM2_ID,
+        "command_channel_id": ROOM2_ID,
         "front": "1",
         "back": "3"
     },
 
     "3": {
         "channel_id": ROOM3_ID,
+        "command_channel_id": ROOM3_ID,
         "front": "2",
         "back": "4"
     },
 
     "4": {
         "channel_id": ROOM4_ID,
+        "command_channel_id": ROOM4_ID,
         "front": "3",
         "back": "5"
     },
 
     "5": {
         "channel_id": ROOM5_ID,
+        "command_channel_id": ROOM5_ID,
         "front": "4",
         "back": "6"
     },
 
     "6": {
         "channel_id": ROOM6_ID,
+        "command_channel_id": ROOM6_THREAD_ID,
         "front": "5",
         "back": None
     }
