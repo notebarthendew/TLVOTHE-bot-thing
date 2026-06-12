af938de18848b3ea31eb3a215371986f54d2f44e
TheSnowyDodo <notebarthendew@gmail.com>
2026-06-11 19:13:06 -0500

    Update helpers.py

--- DiscordBotProject/utils/helpers.py
+++ DiscordBotProject/utils/helpers.py
@@ -1,1 +1,9 @@
 
+from game.player import players
+
+def check_player_status(user_id):
+    if user_id not in players:
+        return "You are not in a train vrotato."
+    if not players[user_id]["alive"]:
+        return "You are dead. *The train rattles on without you.*"
+    return None
