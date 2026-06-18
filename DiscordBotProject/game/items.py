6fe63bb52f2dc5fc66ffeb0e893d04aed8298972
TheSnowyDodoAlt <cardenas.camilo@maximino.edu.co>
2026-06-18 08:46:29 -0500

    Update items.py

--- DiscordBotProject/game/items.py
+++ DiscordBotProject/game/items.py
@@ -1,1 +1,23 @@
-# the code for items
+ITEMS = {
+
+    "knife": {
+
+        "name": "Knife",
+
+        "description": (
+            "A sharp kitchen knife."
+        )
+
+    },
+
+    "sirenenote": {
+
+        "name": "Note",
+
+        "description": (
+            "Friendly note that La Sirene left you with when boarding."
+        )
+
+    }
+
+}
