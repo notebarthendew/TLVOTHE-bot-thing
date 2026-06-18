e5ac56b3722f8fbd18d96a44ebc8fb91ceaca02d
TheSnowyDodoAlt <cardenas.camilo@maximino.edu.co>
2026-06-18 09:53:36 -0500

    Update items.py

--- DiscordBotProject/game/items.py
+++ DiscordBotProject/game/items.py
@@ -6,7 +6,15 @@ ITEMS = {
 
         "description": (
             "A sharp kitchen knife."
-        )
+        ),
+
+        "usable": True,
+
+        "consumable": False,
+
+        # maybe do somethign abt cooldown with import time and time.time()
+        
+        "target_type": "player"
 
     },
 
@@ -15,9 +23,19 @@ ITEMS = {
         "name": "Note",
 
         "description": (
-            "Friendly note that La Sirene left you with when boarding."
-        )
+            "Friendly note that La Siréne left you with when boarding."
+        ),
 
+        "text": "vro ong you aint coming back, believe me, youre so dead its not even funny. jk it is get rekt bozo.",
+
+        "usable": True,
+
+        "consumable": False,
+
+        "target_type": "none"
+        
     }
 
 }
+
+# Available target_type thingamagigs: "none", "player", "room", "corpse", "self".
