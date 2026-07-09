2954c6a69e2fa02854850923a5e2d86e7260baae
TheSnowyDodo <notebarthendew@gmail.com>
2026-07-09 09:50:32 -0500

    Update helpers.py

--- DiscordBotProject/utils/helpers.py
+++ DiscordBotProject/utils/helpers.py
@@ -7,3 +7,13 @@ def check_player_status(user_id):
     if not players[user_id]["alive"]:
         return "You are dead. (Not a big surprise.)"
     return None
+
+def format_time(seconds):
+
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+
+    if hours:
+        return f"{hours}h {minutes}m"
+
+    return f"{minutes}m"
